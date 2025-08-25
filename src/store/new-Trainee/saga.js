1a763140aae4495c8276a1cd23bb589e3aa9bf08
kihwahabtlais <wahab.tlais@keyinhands.com>
2025-08-25 10:11:20 +0300

    edit

--- src/store/new-Trainee/saga.js
+++ src/store/new-Trainee/saga.js
@@ -138,9 +138,8 @@ function* fetchTrainees(selectedpayload) {
     source: "db",
     procedure: "Generic_Optiondatalist",
     apikey: "30294470-b4dd-11ea-8c20-b036fd52a43e",
-    tablename: "AdmissionSettings_RegisterUnderCertificates",
+    tablename: "Settings_DiplomaType",
     fields: `Id,${titleField}`,
-    filter: `checkLevel = 1`,
   };
 
   try {
